51201cbfbf13f2021fa28dc7e3be112261484ec7
Kapil9861 <66173137+Kapil9861@users.noreply.github.com>
2024-06-07 10:40:52 +0545

    added the website icon in the tab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DESIGNERS NEPAL</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
     <link href="{{ secure_asset('css/componentStyling.css') }}" rel="stylesheet">
     <link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
